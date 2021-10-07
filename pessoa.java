905346819f3d9a03cd65e4c70cf6a7763db41541
Yandzick <yandzick@gmail.com>
2021-10-06 23:49:28 -0300

    Create pessoa.java

---
+++ pessoa.java
@@ -0,0 +1,19 @@
+package Prova;
+
+public abstract class Pessoa {
+    private String nome;
+
+    public Pessoa() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Pessoa(String nome){}
+}
