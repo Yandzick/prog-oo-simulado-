60300cfe905789a53420c394e9fd6bc3622da0bb
Yandzick <yandzick@gmail.com>
2021-10-06 23:48:05 -0300

    Create funcionariotester.java

---
+++ funcionariotester.java
@@ -0,0 +1,21 @@
+package Prova;
+
+import java.util.ArrayList;
+
+public class FuncionarioTester {
+
+    public static void main(String[] args) {
+
+
+        Pessoa a = new FuncionarioHorista("ana","01");
+
+        ArrayList<String> listaFunc = new ArrayList();
+        listaFunc.add("");
+        listaFunc.add("");
+        listaFunc.add("");
+        listaFunc.add("");
+
+        System.out.println(listaFunc);
+
+    }
+}
